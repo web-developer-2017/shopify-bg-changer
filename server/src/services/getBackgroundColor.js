ad444179ba0a5c0122fc7332a83a5553dc72ca45
Sergey Kargin <webworkbestservice@gmail.com>
2021-05-10 16:59:46 +0300

    add app on/off

--- server/src/services/getBackgroundColor.js
+++ server/src/services/getBackgroundColor.js
@@ -6,40 +6,44 @@ const getBackgroundColor = async (shop, productVendor) => {
   const globalBackgroundSettings = await getGlobalBackgroundSettingsByShop(shop);
   const backgroundSettings = await getBackgroundSettingsByShop(shop);
   let backgroundColor = '';
-  if(backgroundSettings.length > 0) {
-    for(var i = 0; i < backgroundSettings.length; i++) {
-      if(backgroundSettings[i].vendor == productVendor && backgroundSettings[i].onoff) {
-        backgroundColor = backgroundSettings[i].bgColor;
-        break;
+  if(globalBackgroundSettings[0].appOnOff) {
+    if(backgroundSettings.length > 0) {
+      for(var i = 0; i < backgroundSettings.length; i++) {
+        if(backgroundSettings[i].vendor == productVendor && backgroundSettings[i].onoff) {
+          backgroundColor = backgroundSettings[i].bgColor;
+          break;
+        }
       }
     }
-  }
-
-  if(backgroundColor == '') {
-    if(globalBackgroundSettings[0].onoff) {
-      backgroundColor = globalBackgroundSettings[0].bgColor
-    } else {
-      return false;
+  
+    if(backgroundColor == '') {
+      if(globalBackgroundSettings[0].onoff) {
+        backgroundColor = globalBackgroundSettings[0].bgColor
+      } else {
+        return false;
+      }
     }
+  
+    const backgroundColorSplit = backgroundColor.split(',');
+    let hexRed = parseInt(backgroundColorSplit[0]).toString(16);
+    let hexGreen = parseInt(backgroundColorSplit[1]).toString(16);
+    let hexBlue = parseInt(backgroundColorSplit[2]).toString(16);
+    let hexAlpha = Math.round(backgroundColorSplit[3] * 255).toString(16);
+  
+    if (hexRed.length == 1)
+      hexRed = "0" + hexRed;
+    if (hexGreen.length == 1)
+      hexGreen = "0" + hexGreen;
+    if (hexBlue.length == 1)
+      hexBlue = "0" + hexBlue;
+    if (hexAlpha.length == 1)
+      hexAlpha = "0" + hexAlpha;
+    backgroundColor = hexRed + hexGreen + hexBlue + hexAlpha;
+  
+    return backgroundColor;
+  } else {
+    return false;
   }
-
-  const backgroundColorSplit = backgroundColor.split(',');
-  let hexRed = parseInt(backgroundColorSplit[0]).toString(16);
-  let hexGreen = parseInt(backgroundColorSplit[1]).toString(16);
-  let hexBlue = parseInt(backgroundColorSplit[2]).toString(16);
-  let hexAlpha = Math.round(backgroundColorSplit[3] * 255).toString(16);
-
-  if (hexRed.length == 1)
-    hexRed = "0" + hexRed;
-  if (hexGreen.length == 1)
-    hexGreen = "0" + hexGreen;
-  if (hexBlue.length == 1)
-    hexBlue = "0" + hexBlue;
-  if (hexAlpha.length == 1)
-    hexAlpha = "0" + hexAlpha;
-  backgroundColor = hexRed + hexGreen + hexBlue + hexAlpha;
-
-  return backgroundColor;
 };
 
 module.exports = getBackgroundColor;
